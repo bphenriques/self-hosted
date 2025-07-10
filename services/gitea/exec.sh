1c18b7a5cf553016968c540b5d93dd1e87eb2367
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2025-07-10 18:11:15 +0100

    WIP

---
+++ services/gitea/exec.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+case "${1:-}" in
+  backup)
+    # FIXME it has to be shutdown during
+    shift
+    # https://github.com/sissbruecker/linkding/blob/master/docs/backup.md#database
+    test -d "$1" || error "Not a directory or does not exist: $1"
+
+    docker exec --user git gitea /app/gitea/gitea dump --file /app/gitea/gitea-dump.zip
+    docker exec --user git gitea rm -f /app/gitea/gitea-dump.zip
+    docker cp gitea:/app/gitea/gitea-dump.zip "$1"/gitea.zip
+    ;;
+  *)
+    echo "Unknown command"
+    exit 1
+    ;;
+esac
