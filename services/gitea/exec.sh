098780eeaa2c9089181fda115a33e32a3861776e
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2025-07-10 18:15:27 +0100

    WIP

--- services/gitea/exec.sh
+++ services/gitea/exec.sh
@@ -7,8 +7,8 @@ case "${1:-}" in
     # https://github.com/sissbruecker/linkding/blob/master/docs/backup.md#database
     test -d "$1" || error "Not a directory or does not exist: $1"
 
-    docker exec --user git gitea /app/gitea/gitea dump --file /app/gitea/gitea-dump.zip
     docker exec --user git gitea rm -f /app/gitea/gitea-dump.zip
+    docker exec --user git gitea /app/gitea/gitea dump --file /app/gitea/gitea-dump.zip
     docker cp gitea:/app/gitea/gitea-dump.zip "$1"/gitea.zip
     ;;
   *)
