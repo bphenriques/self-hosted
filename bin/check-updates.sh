9b93f7f1efc40b36ecd644f4757df5627123dd12
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2025-07-18 21:50:24 +0100

    Fix repo location

--- bin/check-updates.sh
+++ bin/check-updates.sh
@@ -5,4 +5,4 @@ cd "$SCRIPT_PATH"/.. || exit 2
 
 test -z "${RENOVATE_TOKEN}" && echo "RENOVATE_TOKEN is not set!" && exit 1
 
-docker run --rm -e RENOVATE_TOKEN="${RENOVATE_TOKEN}" renovate/renovate:38 bphenriques/home-server --onboarding false
+docker run --rm -e RENOVATE_TOKEN="${RENOVATE_TOKEN}" renovate/renovate:38 bphenriques/self-hosted --onboarding false
