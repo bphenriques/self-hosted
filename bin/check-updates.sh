8e0323173a4f59f65839d89898036ad5fbae2e55
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2025-10-10 10:33:02 +0100

    Minor updates

--- bin/check-updates.sh
+++ bin/check-updates.sh
@@ -3,6 +3,9 @@ SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 set -ef
 cd "$SCRIPT_PATH"/.. || exit 2
 
+# renovate/renovate:41.144
+RENOVATE_VERSION="sha256:6fdc3d455dfbf656a34861121fff41cdcfce92b95afbc77524a5f6b071fd3032"
+
 test -z "${RENOVATE_TOKEN}" && echo "RENOVATE_TOKEN is not set!" && exit 1
 
-docker run --rm -e RENOVATE_TOKEN="${RENOVATE_TOKEN}" renovate/renovate:38 bphenriques/self-hosted --onboarding false
+docker run --rm \-e RENOVATE_TOKEN="${RENOVATE_TOKEN}" "renovate/renovate:${RENOVATE_VERSION}" bphenriques/self-hosted --onboarding false
