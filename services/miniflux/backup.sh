8690f0a4539870e3741cfc758d5e2ca606435174
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2025-10-29 15:55:12 +0000

    WIP

--- services/miniflux/backup.sh
+++ services/miniflux/backup.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # https://miniflux.app/docs/api.html#endpoint-export
 
-test -d "$1" || error "Not a directory or does not exist: $1"
+if ! test -d "$1"; then
+  echo "Not a directory or does not exist: $1"
+  exit 1
+fi
+
 target_dir="$1"
 echo "Backing up miniflux: ${target_dir}"
 mkdir -p "${target_dir}"
