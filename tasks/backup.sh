a20a6dff0b9189925531b4339e973be59d3596eb
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2025-10-30 16:28:09 +0000

    finetune

--- tasks/backup.sh
+++ tasks/backup.sh
@@ -18,19 +18,27 @@ backup() {
     home-server task "$service" backup "$RUSTIC_BACKUP_EXTRA_FILES" || fatal "Backup $service failed!"
   done
 
+  echo "--------"
+  echo "Backup folder ready for upload: $RUSTIC_BACKUP_EXTRA_FILES'"
+  echo "--------"
   ls -la "$RUSTIC_BACKUP_EXTRA_FILES"
-  echo "Backup folder ready for upload: $RUSTIC_BACKUP_EXTRA_FILES"
 
   rustic backup
   rustic forget
-  rustic check #--read-data-subset=nS -> daily backup means 30 runs -> lets chunk by 30.
+  
+  # To be fine-tuned:
+  # - I dont change the data _that_ often.
+  # - Backblaze allows up 3 times the average of the data stored as egress.
+  # - Backup happens daily.
+  # - But..  downloading >1GB per days seem excessive and after a while, this value seems reasonable.
+  rustic check --read-data-subset=100MB
 }
 
 RUSTIC_REPOSITORY="$1"
 shift
 
 export RUSTIC_BACKUP_EXTRA_FILES="$(mktemp -d --suffix -backup)"
-echo "Running rustic for repository: $RUSTIC_REPOSITORY and working directory '$RUSTIC_BACKUP_EXTRA_FILES'"
+echo "Running rustic for repository: '$RUSTIC_REPOSITORY' and working directory '$RUSTIC_BACKUP_EXTRA_FILES'"
 
 case "${1:-}" in
   backup)   backup                ;;
