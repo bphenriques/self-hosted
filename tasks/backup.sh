15a71ce817cc8e54d767111976b15abb00cc1c27
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2025-10-30 16:36:48 +0000

    Print and increase data subset

--- tasks/backup.sh
+++ tasks/backup.sh
@@ -27,24 +27,27 @@ backup() {
 
   echo "--------"
   echo "Forgetting and pruning data..."
-  echo "--------"  
+  echo "--------"
+  echo  
   rustic forget
 
   echo "--------"
   echo "Checking integrity of the repository..."
   echo "--------"
+  echo
   rustic check
  
   echo "--------"
   echo "Checking integrity of the data (higher bandwidth)..."
-  echo "--------"  
+  echo "--------"
+  echo
   
   # To be fine-tuned:
   # - I dont change the data _that_ often.
   # - Backblaze allows up 3 times the average of the data stored as egress.
   # - Backup happens daily.
   # - But..  downloading >1GB per days seem excessive and after a while, this value seems reasonable.
-  rustic check --read-data --read-data-subset=100MB
+  rustic check --read-data --read-data-subset=500MB
 }
 
 RUSTIC_REPOSITORY="$1"
